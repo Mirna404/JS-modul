eafb15667f66e5ee81676e9335b121e8e32f7cdd
Mirna <mirna.busic9@gmail.com>
2022-12-12 00:08:04 +0100

    second homework

---
+++ 2/homework2.js
@@ -0,0 +1,28 @@
+const prviAuto = {
+	ime: document.getElementById("nickname1"),
+	brzina: (brzina1 = document.getElementById("speed1")),
+	udaljenost: document.getElementById("distance"),
+};
+
+const drugiAuto = {
+	ime: document.getElementById("nickname2"),
+	brzina: (brzina1 = document.getElementById("speed2")),
+	udaljenost: document.getElementById("distance"),
+};
+
+let pobjednik = document.getElementById("results");
+
+function tkoJeBrzi(event) {
+	event.preventDefault();
+	rezultatPrvi = prviAuto.udaljenost.value / prviAuto.brzina.value;
+	rezultatDrugi = drugiAuto.udaljenost.value / drugiAuto.brzina.value;
+	if (rezultatDrugi > rezultatPrvi) {
+		pobjednik.innerHTML = ` ${prviAuto.ime.value} je brži od ${drugiAuto.ime.value}.`;
+	} else if (rezultatPrvi > rezultatDrugi) {
+		pobjednik.innerHTML = ` ${drugiAuto.ime.value} je brži od ${prviAuto.ime.value}.`;
+	} else if ((rezultatPrvi = rezultatDrugi)) {
+		pobjednik.innerHTML = `${prviAuto.ime.value} i ${drugiAuto.ime.value} stigli su u isto vrijeme!`;
+	} else {
+		pobjednik.innerHTML = `Pliz ispravno ispuni predviđene forme. Tenks!`;
+	}
+}
